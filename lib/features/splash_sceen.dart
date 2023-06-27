a898b12e86ea036b94fbf0d13f7ca52b832c4a24
Chamika Dilshan <chamika.dilshan@app360.lk>
2023-06-27 21:26:39 +0530

    added rotate animation in splash screen

--- lib/features/splash_sceen.dart
+++ lib/features/splash_sceen.dart
@@ -2,8 +2,34 @@ import 'package:donut_shop_app/constants/utils.dart';
 import 'package:donut_shop_app/features/donut_shop_main.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> with SingleTickerProviderStateMixin{
+
+  AnimationController? donutController;
+  Animation<double>? rotateAnimation;
+
+  @override
+  void initState(){
+    super.initState();
+    donutController = AnimationController(
+      duration: Duration(seconds: 5),
+        vsync: this
+    )..repeat();
+    rotateAnimation = Tween<double>(begin: 0, end: 1)
+    .animate(CurvedAnimation(parent: donutController!, curve: Curves.linear));
+  }
+
+  @override
+  void dispose(){
+    donutController!.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +49,14 @@ class SplashPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image.network(Utils.donutLogoWhiteNoText, width: 100.0, height: 100.0,),
+            RotationTransition(
+                turns: rotateAnimation!,
+                child: Image.network
+                  (Utils.donutLogoWhiteNoText,
+                  width: 100.0,
+                  height: 100.0,
+                )
+            ),
             Image.network(Utils.donutLogoWhiteText, width: 100.0, height: 100.0,)
           ],
         ),
