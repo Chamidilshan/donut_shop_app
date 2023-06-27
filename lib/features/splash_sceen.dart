140ae2d2286ce46f64d001c4245787e0b9bb1f69
Chamika Dilshan <chamika.dilshan@app360.lk>
2023-06-27 21:12:03 +0530

    added navigation for pages

--- lib/features/splash_sceen.dart
+++ lib/features/splash_sceen.dart
@@ -1,4 +1,5 @@
 import 'package:donut_shop_app/constants/utils.dart';
+import 'package:donut_shop_app/features/donut_shop_main.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
@@ -6,6 +7,15 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+            builder: (context)=> DonutShopMain()
+        )
+      );
+    });
+
     return  Scaffold(
       backgroundColor: Utils.mainColor,
       body: Center(
